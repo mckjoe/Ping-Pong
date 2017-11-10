a18ac7927d4e0cc62f3e1561a47889795ceeb69a
joe mckinney <mckinney.a.joe@gmail.com>
2017-11-10 12:08:35 -0800

    add functionality so that numbers divisible by 15 show up in the list as pingpong

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
-
+// Contents of outputTexts will be displayed in the html after going through a forEach() loop and appended to a ul called "unstyled"
 var outputTexts = [];
 
+ // This function creates an array from 1 - userinput.
 var count = function(userInput) {
   var digits = [];
   for (var i = 1; i <= userInput; i++) {
     digits.push(i);
   }
   return digits
-  }
+}
+
+// This function runs conditions to see what to push into outputTexts
 var conditions = function(results) {
   results.forEach(function(result) {
-    if (result % 3 === 0) {
-      outputTexts.push("ping");
+    if (result % 15 === 0) {
+      outputTexts.push("pingpong")
+    } else if (result % 3 === 0) {
+      outputTexts.push("ping")
     } else if (result % 5 === 0) {
-      outputTexts.push("pong");
+      outputTexts.push("pong")
     } else {
       outputTexts.push(result)
     }
@@ -27,12 +32,13 @@ $(document).ready(function() {
     var userInput = parseInt($("#userInput").val());
     var results = count(userInput);
     var output = conditions(results);
-    // console.log(result);
-    console.log(outputTexts);
 
+    // for each function will append individul items to the "unstyled list"
     outputTexts.forEach(function(outputText) {
       $("#unstyled").append("<li>" + outputText);
+
     });
+    // makes the hidden div show for the users delight
     $("#output").show();
   });
 });
