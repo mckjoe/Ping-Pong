576b992d8c181dbd98aaa5224e6cb21db98d2b22
joe mckinney <mckinney.a.joe@gmail.com>
2017-11-10 10:46:43 -0800

    add to the js so that the program can count to number input

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 
+var count = function(userInput) {
+  var numbers = [];
+  for (var i = 1; i <= userInput; i++) {
+    numbers.push(i);
+  }
+  return numbers
+}
 
 $(document).ready(function() {
   $("#input").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("#userInput").val());
-    console.log(userInput);
+    var result = count(userInput);
+    console.log(result);
 
     $(".outputText").text(userInput);
     $("#output").show();
